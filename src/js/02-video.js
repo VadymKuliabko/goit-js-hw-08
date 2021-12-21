0f72519c26e60beff9e09967ad4f970772818bad
Vadym-Kuliabko <vadilatele@gmail.com>
2021-12-21 21:46:00 +0100

    add files vimeo/player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const storage = 'vedeoplayer-current-time';
+
+player.on('play', onPlay => player.on('timeupdate', throttle(setCurrentTimeStorage, 1000)));
+
+function setCurrentTimeStorage(event) {
+  localStorage.setItem(storage, event.seconds);
+}
+
+function getCurrentTimeStorage() {
+  const storedVideo = localStorage.getItem(storage);
+  if (storedVideo) {
+    player.setCurrentTime(storedVideo);
+  }
+}
+
+getCurrentTimeStorage();
