7537c855a899ebd6c8a1b9dee05df52f783f443c
Vadym-Kuliabko <vadilatele@gmail.com>
2021-12-22 15:07:09 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,38 @@
 import throttle from 'lodash.throttle';
 
-const key = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
-  form: document.querySelector('feedback-form'),
-  input: document.querySelector('feedback-form input'),
-  textarea: document.querySelector('feedback-form textarea'),
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
 };
 
 const formData = {};
-getFormData();
+getStoredFormData();
 
-refs.input.addEventListener('input', throttle(onInput, 500));
 refs.textarea.addEventListener('input', throttle(onInput, 500));
+refs.input.addEventListener('input', throttle(onInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onInput(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem(key, JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function getFormData() {
-  const dataForm = JSON.parse(localStorage.getItem(key));
+function getStoredFormData() {
+  const storedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (dataForm) {
-    refs.textarea.value = dataForm.message;
-    formData.message = dataForm.message;
-    refs.input.value = dataForm.email;
-    formData.email = dataForm.email;
+  if (storedFormData) {
+    refs.textarea.value = storedFormData.message;
+    formData.message = storedFormData.message;
+    refs.input.value = storedFormData.email;
+    formData.email = storedFormData.email;
   }
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
-  localStorage.removeItem(key);
+  localStorage.removeItem(STORAGE_KEY);
 }
-
-console.log(formData.email);
-console.log(formData.message);
